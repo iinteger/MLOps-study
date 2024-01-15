a92a55753455c0f4bd491063fdf8456da1f53c62
iinteger <42987286+iinteger@users.noreply.github.com>
2024-01-15 14:28:51 +0900

    프로그래머스 강의 실습

---
+++ programmers-mlops2/part5/download_data.py
@@ -0,0 +1,19 @@
+from minio import Minio
+
+#
+# minio client
+#
+url = "localhost:9000"
+access_key = "minio"
+secret_key = "miniostorage"
+client = Minio(url, access_key=access_key, secret_key=secret_key, secure=False)
+
+#
+# data download
+#
+bucket_name = "raw-data"
+object_name = "iris"
+
+object_stat = client.stat_object(bucket_name, object_name)
+print(object_stat.version_id)
+client.fget_object(bucket_name, object_name, file_path="download_data.csv")
